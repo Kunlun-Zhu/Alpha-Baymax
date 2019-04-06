023af0b2dd36a575c63fde6c7726d3c7fb1911e1
Kunlun Zhu <516455665@qq.com>
2019-04-06 11:54:30 -0400

    Update Alpha-bay-max_main_kunlun.py

--- Alpha-bay-max_main_kunlun.py
+++ Alpha-bay-max_main_kunlun.py
@@ -14,8 +14,9 @@ ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 question_list = ['question1', 'question2', 'question3', 'question4', 'question5'] #list of question we are going to ask
-positive_list = ['yes', 'yes', 'yes', 'yes', 'yes']
-negetive_list = ['no', 'no', 'no', 'no', 'no']
+positive_list = ['yes', 'yes', 'yes', 'yes', 'yes'] #answer that will increase the final_review
+negetive_list = ['no', 'no', 'no', 'no', 'no'] #answer that will decrease the final_review
+
 
 round_number = 0 #to clarify what number of question we are going to ask
 
@@ -50,11 +51,11 @@ def answer(first, second, third):
 
     winning_numbers = session.attributes['question']
 
-    if first == 'yes':
+    if first == positive_list[round_number-1]:
 
         msg = render_template('win')
         Final_review += 1
-    elif first == 'no':
+    elif first == negetive_list[round_number-1]:
 
         msg = render_template('lose')
         Final_review -= 1
